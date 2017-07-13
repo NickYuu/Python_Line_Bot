0dca524f6b9d418c32949ebb54c51b60ad13bcb8
TsungHan <nick.yu@hzn.com.tw>
2017-07-13 21:10:58 +0800

    make it better

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    res = requests.get('http://chattergg.club?msg=' + event.message.text)
+    res = requests.get('http://chattergg.club/a?msg=' + event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=res.text))
